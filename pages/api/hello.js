b7102bd140d264041f9d92c0c95a7ddc2dd9d2af
Jhon Nieves <jancx031@gmail.com>
2023-10-07 16:05:20 -0400

    works

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// pages/api/proxy.js
+export default async (req, res) => {
+  try {
+    const response = await fetch('http://numbersapi.com/42');
+    const data = await response.text();
+    res.status(200).json({ fact: data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
